1206177eecc4ae55d002fc498ed0fb93c5d4ae21
Hahyunji337 <gaulbaram0111@gmail.com>
2019-10-27 16:42:05 +0900

    191027

---
+++ Project66/Project66/소스.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct sbox { // 구조체sbos의 정의
+	int mem1;
+	int mem2;
+	double mem3;
+} SBox;
+
+typedef union ubox { // 공용체sbos의 정의
+	int mem1;
+	int mem2;
+	double mem3;
+} UBox;
+
+
+int main(void)
+{
+	SBox sbx;
+	UBox ubx;
+
+	printf("%p %p %p \n", &sbx.mem1, &sbx.mem2, &sbx.mem3);
+	printf("%p %p %p \n", &ubx.mem1, &ubx.mem2, &ubx.mem3);
+	printf("%d %d \n", sizeof(sbx), sizeof(ubx));
+
+	return 0;
+}
