2073092a868aa5805da3fec6666ffd842facb16d
Hahyunji337 <gaulbaram0111@gmail.com>
2019-11-15 00:08:20 +0900

    191115

--- Project78/Project78/소스.c
+++ Project78/Project78/소스.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
-#define ADD 1
-#define MIN 0
+//#define ADD 1
+//#define MIN 0
+#define STRING_JOB(A, B) #A"의 직업은 " #B "입니다."
 
 int main(void) {
-	int num1, num2;
-	printf("두 개의 정수 입력 \n");
-	scanf("%d %d", &num1, &num2);
+	printf("%s", STRING_JOB("구정모", "갓수"));
+	//int num1, num2;
+	//printf("두 개의 정수 입력 \n");
+	//scanf("%d %d", &num1, &num2);
 
-#if ADD // ADD가 참이면
+#ifdef ADD // ADD가 정의되었다면
 	printf("%d + %d = %d \n", num1, num2, num1 + num2);
 #endif
 
-#if MIN // MIN이 참이면
+#ifdef MIN // MIN이 정의되었다면
 	printf("%d - %d = %d \n", num1, num2, num1 - num2);
 #endif
+
+	
+
 
 	return 0;
 }
