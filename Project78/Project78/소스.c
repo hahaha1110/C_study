392516d758b1798c43e0e223802acc8eb34b037d
Hahyunji337 <gaulbaram0111@gmail.com>
2019-11-15 00:13:58 +0900

    191115

--- Project78/Project78/소스.c
+++ Project78/Project78/소스.c
@@ -2,9 +2,12 @@
 //#define ADD 1
 //#define MIN 0
 #define STRING_JOB(A, B) #A"의 직업은 " #B "입니다."
-
+#define CON(UPP, LOW) UPP ## 00 ## LOW
+#define CON2(UPP, LOW) UPP ## LOW
 int main(void) {
-	printf("%s", STRING_JOB("구정모", "갓수"));
+	printf("%d \n", CON(101, 200));
+	printf("%d \n", CON2(101, 200));
+	//printf("%s", STRING_JOB("구정모", "갓수"));
 	//int num1, num2;
 	//printf("두 개의 정수 입력 \n");
 	//scanf("%d %d", &num1, &num2);
